a838e1111c80a15be2658c453ed2f42fe1f20094
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-01-17 15:58:15 +0200

    ...

--- test-addon/run.sh
+++ test-addon/run.sh
@@ -1,13 +1,3 @@
-# run.sh
-#!/usr/bin/with-contenv bashio
-
+#!/usr/bin/env bash
 echo "Starting Flask on 0.0.0.0:5000"
-
-python3 -m venv /venv
-. /venv/bin/activate
-pip install --no-cache-dir flask
-
-cd /data/www
-
-# Test simple server
-python -m flask run --host=0.0.0.0 --port=5000
+python3 -m flask run --host=0.0.0.0 --port=5000
