c2b34db17b66287f7ba8c308dd772679c13adc2f
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-01-17 15:28:57 +0200

    ..

--- test-addon/run.sh
+++ test-addon/run.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/env bash
-echo "Starting Test Addon..."
+#!/usr/bin/with-contenv bashio
 
-python3 - <<'EOF'
-from flask import Flask, send_from_directory
-
-app = Flask(__name__, static_folder="www")
-
-@app.route("/")
-def index():
-    return send_from_directory(app.static_folder, "index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-EOF
+echo "Hello world!"
