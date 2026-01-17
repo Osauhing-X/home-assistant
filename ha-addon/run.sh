c2d4730863b75882eb3f1ec3a5e2f7f61772ab8e
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-01-17 20:01:22 +0200

    1.0.16

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -3,13 +3,32 @@ set -e
 
 echo "Starting ESP32 BLE Presence Addon..."
 
-# Aktiveeri virtual environment
+# Aktiveeri Python virtual environment
 . /venv/bin/activate
 
 # Käivitame Flask serveri taustas
 echo "Starting Flask server..."
 python3 /server.py &
 
+# Lighttpd konfiguratsioon
+echo "Configuring Lighttpd..."
+mkdir -p /var/www/localhost/htdocs
+cat <<EOF >/etc/lighttpd/lighttpd.conf
+server.port = 8099
+server.bind = "0.0.0.0"
+server.document-root = "/var/www/localhost/htdocs"
+server.modules = ("mod_accesslog")
+index-file.names = ("index.html")
+accesslog.filename = "/var/log/lighttpd/access.log"
+EOF
+
+# Kopeeri front-end failid
+if [ -d "/www" ]; then
+    cp -r /www/* /var/www/localhost/htdocs/
+else
+    echo "Warning: /www folder missing!"
+fi
+
 # Käivitame Lighttpd foregroundis
 echo "Starting Lighttpd ingress UI..."
-python3 /webui.py
+exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
