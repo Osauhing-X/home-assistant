6d7412125e4d5eb1172f04e5321e3bf674ed2808
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-01-17 18:01:09 +0200

    ..

--- test-addon/run.sh
+++ test-addon/run.sh
@@ -2,4 +2,12 @@
 
 echo "Starting Lighttpd ingress UI on port 8099"
 
+cat <<EOF >/etc/lighttpd/lighttpd.conf
+server.port = 8099
+server.bind = "0.0.0.0"
+server.document-root = "/var/www/localhost/htdocs"
+server.modules = ("mod_accesslog")
+index-file.names = ("index.html")
+EOF
+
 lighttpd -D -f /etc/lighttpd/lighttpd.conf
