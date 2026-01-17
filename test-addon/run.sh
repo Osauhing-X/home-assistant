801d4c5799ac17f63880b4caf7701f112aa26e72
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-01-17 16:00:41 +0200

    ..

--- test-addon/run.sh
+++ test-addon/run.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/env bash
-echo "Starting Flask on 0.0.0.0:5000"
-python3 -m flask run --host=0.0.0.0 --port=5000
+#!/usr/bin/with-contenv bashio
+
+echo "Starting Flask web server..."
+
+# Set Flask app environment
+export FLASK_APP=/data/www/app.py
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_RUN_PORT=5000
+
+# Run Flask
+python3 -m flask run
