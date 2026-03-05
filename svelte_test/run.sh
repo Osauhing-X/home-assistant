b7f8762a934fd0bec90ef8a5e7dfe8946cbe8655
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-03-05 11:19:29 +0200

    .env

--- svelte_test/run.sh
+++ svelte_test/run.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+
 # määrame Node serveri porti
 export PORT=8080
 
-# loe HA config.yaml otse bashio kaudu
-URL=$(bashio::config 'api_url')
-INTERVAL=$(bashio::config 'refresh_interval')
 
 # tee .env fail SvelteKit brauserile
 cat > .env <<EOF
-VITE_ADDON_API_URL="${URL}"
-VITE_ADDON_REFRESH_INTERVAL="${INTERVAL}"
+PUBLIC_URL="${bashio::config 'api_url'}"
+PUBLIC_REFRESH="${bashio::config 'refresh_interval'}"
 EOF
+
 
 echo ".env created:"
 cat .env
+
 
 echo "Starting SvelteKit Node server on port $PORT..."
 node build/index.js
--- svelte_test/src/lib/config.js
+++ svelte_test/src/lib/config.js
@@ -1,3 +1,5 @@
 // src/config.js
-export const ADDON_API_URL = import.meta.env.VITE_ADDON_API_URL || "";
-export const ADDON_REFRESH_INTERVAL = import.meta.env.VITE_ADDON_REFRESH_INTERVAL || "";
+import { env } from '$env/dynamic/public';
+
+export const ADDON_API_URL = env.PUBLIC_URL ?? "";
+export const ADDON_REFRESH_INTERVAL = env.PUBLIC_REFRESH ?? "";
