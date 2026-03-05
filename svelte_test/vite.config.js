89fc1e862795ccc3d964a4a3727b10bf74420b9c
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-03-05 03:13:56 +0200

    fix port -> 8080

--- svelte_test/vite.config.js
+++ svelte_test/vite.config.js
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+    	host: true,   // kõik IP-d, et konteineris kättesaadav
+    	port: 8080    // Home Assistant ingress port
+  	}
 });
