efb3cb5c78f32a5ab4bd7d8c6624a9f0a771c9cd
VØ!D <86025696+t44v1@users.noreply.github.com>
2026-03-05 04:17:09 +0200

    env v2

--- svelte_test/run.sh
+++ svelte_test/run.sh
@@ -5,13 +5,13 @@ set -e
 export PORT=8080
 
 # Kui keskkonnamuutujad puuduvad, jäta tühjaks
-VITE_ADDON_API_URL="${ADDON_API_URL-}"
-VITE_ADDON_REFRESH_INTERVAL="${ADDON_REFRESH_INTERVAL-}"
+export URL="${ADDON_API_URL:-}"
+export INTERVAL="${ADDON_REFRESH_INTERVAL:-}"
 
 # tee .env fail SvelteKit buildi jaoks
 cat > .env <<EOF
-VITE_ADDON_API_URL="${ADDON_API_URL}"
-VITE_ADDON_REFRESH_INTERVAL="${ADDON_REFRESH_INTERVAL}"
+VITE_ADDON_API_URL="${URL}"
+VITE_ADDON_REFRESH_INTERVAL="${INTERVAL}"
 EOF
 
 echo ".env created:"
