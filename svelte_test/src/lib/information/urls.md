1d2ccbf24af1f7b7dda5daea98453ef39b8b6a00
t44v1 <taavitoodu+github@gmail.com>
2026-03-08 20:17:15 +0200

    testing...

--- svelte_test/src/lib/information/urls.md
+++ svelte_test/src/lib/information/urls.md
@@ -2,12 +2,16 @@
   import { base } from '$app/paths';
   <a href={base + '/sv'}>Movies 2</a>
 
+  window.location.href = `${base}${path}`;
+
   base -> "/api/hassio_ingress/0tePT97nBNgQv0oOBr9A_N6w0B6xR4ru2vLXJsQqIO0" + "/sv"
 ```
 
 ```
   import { resolve } from '$app/paths';
   <a href={resolve('/sv')}>Movies 2</a>
+
+  window.location.href = resolve('/s_all');
 
   resolve -> "http://home.local:8123/api/hassio_ingress/0tePT97nBNgQv0oOBr9A_N6w0B6xR4ru2vLXJsQqIO0/sv"
 ```
--- svelte_test/src/routes/+page.svelte
+++ svelte_test/src/routes/+page.svelte
@@ -7,26 +7,7 @@
   import Calender from "$lib/pages/home/calender.svelte";
 
 
-   import { resolve } from '$app/paths';
 
-  function goToMovies() {
-    window.location.href = resolve('/s_all');
-  }
-
-    function goToMovies2() {
-    window.location.assign(resolve('/sv'))
-  }
-
-
-    function goToMovies3() {
-    window.location.replace(resolve('/sv'));
-  }
-
-  import { base } from '$app/paths';
-
-  function navigate(path) {
-    window.location.href = `${base}${path}`;
-  }
 </script>
 
 
@@ -45,12 +26,3 @@
 <hr>
 
 <Calender />
-
-<hr>
-
-<center class="flex wrap gap">
-  <button on:click={goToMovies}>Test 1</button>
-  <button on:click={() => navigate('/s_all')}>Test 2</button>
-  <button on:click={goToMovies2}>test 3</button>
-  <button on:click={goToMovies3}>test 4</button>
-</center>
