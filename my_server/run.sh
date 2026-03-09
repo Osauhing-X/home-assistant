3ae71566206ac3fa7c7130ef2e047d37364b0648
t44v1 <taavitoodu+github@gmail.com>
2026-03-09 19:48:50 +0200

    ...

--- my_server/run.sh
+++ my_server/run.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/with-contenv bashio
+set -e
+
+# Kloonime priv repo runtime
+REPO=$(bashio::config 'repo')
+TOKEN=$(bashio::config 'github_token')
+
+echo "Removing old app..."
+rm -rf /app/*
+
+echo "Cloning private repo..."
+git clone --depth 1 https://$TOKEN@github.com/$REPO /app
+
+echo "Installing dependencies..."
 cd /app
-# Node foreground PID 1 all
+npm install --omit=dev
+
+# Node foreground PID 1
 node index.js
