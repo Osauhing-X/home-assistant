9576124a0e3f84cba2ddc22d5c38e1928a3088d1
t44v1 <taavitoodu+github@gmail.com>
2026-03-09 18:51:35 +0200

    ...

--- node_server/run.sh
+++ node_server/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/with-contenv bashio
 
-REPO=$(bashio::config 'repo')
+# Config from HA
+REPO=$(bashio::config 'repo')        # username/name.git
 TOKEN=$(bashio::config 'github_token')
 
 echo "Removing old app..."
-rm -rf /app
+rm -rf /app/*
 
 echo "Cloning private repo..."
-git clone --depth 1 https://$TOKEN@github.com/$REPO /app
+git clone --depth 1 https://$TOKEN@github.com/$REPO.git /app
 
 echo "Installing dependencies..."
 cd /app
