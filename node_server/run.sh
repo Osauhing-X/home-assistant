85a458b42e9fd32c7a0dd9f44c8b652aec8d9230
t44v1 <taavitoodu+github@gmail.com>
2026-03-09 18:44:10 +0200

    feat: migrate Node server files to new directory structure and update configuration

---
+++ node_server/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+
+REPO=$(bashio::config 'repo')
+TOKEN=$(bashio::config 'github_token')
+
+echo "Removing old app..."
+rm -rf /app
+
+echo "Cloning private repo..."
+git clone --depth 1 https://$TOKEN@github.com/$REPO /app
+
+echo "Installing dependencies..."
+cd /app
+npm install --omit=dev
+
+echo "Starting Node server..."
+node index.js
