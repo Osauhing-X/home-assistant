0edb2612ec8d09d5d99197508db47f7cb27ef632
t44v1 <taavitoodu+github@gmail.com>
2026-03-23 22:04:24 +0200

    test code - 2

--- setup/plugins/template/switch.py
+++ setup/plugins/template/switch.py
@@ -1,9 +1,11 @@
+import requests
 from homeassistant.components.switch import SwitchEntity
 
 class MySwitch(SwitchEntity):
-    def __init__(self, name):
+    def __init__(self, name, id):
         self._name = name
         self._state = False
+        self._id = id
 
     @property
     def name(self):
@@ -15,10 +17,14 @@ class MySwitch(SwitchEntity):
 
     async def async_turn_on(self, **kwargs):
         self._state = True
+        try:
+            requests.post(f"http://<nodejs_ip>:3000/switch/{self._id}", json={"action": "on"})
+        except:
+            pass
 
     async def async_turn_off(self, **kwargs):
         self._state = False
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    entities = [MySwitch(f"Relay {i+1}") for i in range(3)]
-    async_add_entities(entities)
+        try:
+            requests.post(f"http://<nodejs_ip>:3000/switch/{self._id}", json={"action": "off"})
+        except:
+            pass
