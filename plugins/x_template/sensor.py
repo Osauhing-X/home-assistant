a3307e03b23d2d7e69af506ccf1aeb6c3b1b5049
t44v1 <taavitoodu+github@gmail.com>
2026-03-25 00:25:22 +0200

    works...

--- plugins/x_template/sensor.py
+++ plugins/x_template/sensor.py
@@ -8,7 +8,8 @@ class XTemplateNodeSensor(SensorEntity):
     def __init__(self, node_name):
         self.node_name = node_name
         self._attr_name = f"{node_name} Status"
-        self._attr_icon = "/local/x_template/icon.png"
+        self._attr_icon = "mdi:server-network"  # tavaline mdi ikoon
+        self._attr_unique_id = f"x_{node_name.lower()}"  # lühem ja selgem
 
     @property
     def native_value(self):
