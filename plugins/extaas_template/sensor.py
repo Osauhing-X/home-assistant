fa1cb053d3e2bd5d46fbde01ee712e7a52347cba
t44v1 <taavitoodu+github@gmail.com>
2026-03-25 23:09:19 +0200

    ...

--- plugins/extaas_template/sensor.py
+++ plugins/extaas_template/sensor.py
@@ -1,88 +1,34 @@
-import logging
+# sensor.py
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-_LOGGER = logging.getLogger(__name__)
+from .const import DOMAIN
 
-DOMAIN = "extaas_template"
-_ENTITIES = {}  # entity_id -> ExTaaS_Sensor
-
-def update_entities(service_payload):
-    """
-    Uuenda teenuse sensorid.
-    Payload peab olema kujul:
-    {
-        "node": "taavi-book-13",
-        "service": "Website",
-        "host": "10.1.1.74",
-        "port": 3010,
-        "data": [{ 
-           "name": "web",
-           "value": true,
-           "icon": "mdi:connection",
-           "device_class": "connectivity" },
-           ... ]}
-    """
-    node_name = service_payload.get("node")
-    service_name = service_payload.get("service")
-    host = service_payload.get("host")
-    port = service_payload.get("port")
-    data_list = service_payload.get("data", [])
-
-    if not node_name or not service_name:
-        _LOGGER.warning("Payloadil puudub node või service nimi")
-        return
-
-    for item in data_list:
-        name = item.get("name")
-        if not name:
-            continue
-
-        value = item.get("value")
-        icon = item.get("icon", "mdi:checkbox-blank-outline")
-        device_class = item.get("device_class")
-
-        entity_id = f"{node_name}_{service_name}_{name}".lower().replace(" ", "_")
-        if entity_id not in _ENTITIES:
-            _ENTITIES[entity_id] = ExTaaS_Sensor(
-                node_name=node_name,
-                service_name=service_name,
-                sensor_name=name,
-                value=value,
-                icon=icon,
-                device_class=device_class,
-                host=host,
-                port=port
-            )
-        else:
-            _ENTITIES[entity_id].update_data(value, icon, device_class, host, port)
-
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Lisab kõik olemasolevad sensorid HA-sse."""
-    async_add_entities(_ENTITIES.values())
-
-
-class ExTaaS_Sensor(Entity):
-    def __init__(self, node_name, service_name, sensor_name, value, icon, device_class, host=None, port=None):
-        self.node_name = node_name
-        self.service_name = service_name
-        self.sensor_name = sensor_name
-        self._value = value
-        self._icon = icon
+class ExTaaSSensor(Entity):
+    def __init__(self, node_name, host, port, service_name, name, value, icon=None, device_class=None):
+        self._node_name = node_name
+        self._host = host
+        self._port = port
+        self._service_name = service_name
+        self._name = name
+        self._state = value
+        self._icon = icon or "mdi:checkbox-blank-outline"
         self._device_class = device_class
-        self.host = host
-        self.port = port
 
-        self._attr_name = f"{node_name} ← {service_name}"
-        self._attr_unique_id = f"{node_name}_{service_name}_{sensor_name}".lower().replace(" ", "_")
+    @property
+    def name(self):
+        # Kuvab: Node ← Teenus (IP:Port)
+        return f"{self._node_name} ← {self._service_name} ({self._host}:{self._port})"
 
     @property
     def state(self):
-        return self._value
+        return self._state
 
     @property
     def icon(self):
-        return self._icon or "mdi:checkbox-blank-outline"
+        return self._icon
 
     @property
     def device_class(self):
@@ -91,18 +37,33 @@ class ExTaaS_Sensor(Entity):
     @property
     def extra_state_attributes(self):
         return {
-            "host": self.host,
-            "port": self.port,
-            "sensor_name": self.sensor_name
+            "host": self._host,
+            "port": self._port,
+            "sensor_name": self._name
         }
 
-    def update(self):
-        """HA kutsub update, sensoril pole midagi lisaks teha"""
-        pass
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+    """Set up sensors from config entry / discovery_info."""
+    node = entry.data  # peaks sisaldama: node_name, host, port, service_name, node_data
+    node_name = node.get("node_name")
+    host = node.get("host")
+    port = node.get("port")
+    service_name = node.get("service_name")
+    node_data = node.get("node_data", [])
 
-    def update_data(self, value, icon=None, device_class=None, host=None, port=None):
-        self._value = value
-        self._icon = icon or "mdi:checkbox-blank-outline"
-        self._device_class = device_class
-        self.host = host
-        self.port = port
+    sensors = []
+    for item in node_data:
+        sensors.append(
+            ExTaaSSensor(
+                node_name=node_name,
+                host=host,
+                port=port,
+                service_name=service_name,
+                name=item["name"],
+                value=item["value"],
+                icon=item.get("icon"),
+                device_class=item.get("device_class"),
+            )
+        )
+
+    async_add_entities(sensors, update_before_add=True)
