42c9adf99251853faf24b01a1da945429b564dfb
t44v1 <taavitoodu+github@gmail.com>
2026-03-31 00:56:58 +0300

    fix

--- plugins/extaas_template/switch.py
+++ plugins/extaas_template/switch.py
@@ -1,5 +1,5 @@
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from .entities import ExtaasSensor
+from .entities import ExtaasSwitch
 from .const import DOMAIN, SIGNAL_UPDATE
 
 
@@ -10,26 +10,26 @@ async def async_setup_entry(hass, entry, async_add_entities):
         if entry_id != entry.entry_id:
             return
 
-        storage = hass.data[DOMAIN]["storage"]
+        storage = hass.data[DOMAIN].get("storage", {})
         entry_data = storage.get(entry.entry_id, {})
         data = entry_data.get("entities", {})
 
         new = []
 
         for k, v in data.items():
-            if v.get("type") != "sensor":
+            if v.get("type") != "switch":
                 continue
 
             if k not in entities:
-                ent = ExtaasSensor(hass, entry, k)
+                ent = ExtaasSwitch(hass, entry, k)
                 entities[k] = ent
                 new.append(ent)
 
         if new:
             async_add_entities(new)
 
-    # initial load
+    # 👉 initial load
     add_missing_entities(entry.entry_id, set())
 
-    # listen updates
+    # 👉 listen for updates
     async_dispatcher_connect(hass, SIGNAL_UPDATE, add_missing_entities)
